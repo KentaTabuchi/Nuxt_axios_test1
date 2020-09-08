10c1d82478bd4b4677fd056082b5274cb330c32d
田淵健太 <>
2020-09-08 23:50:45 +0900

    succeed in ajax.

---
+++ pages/axios.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+    <pre class="text-light bg-dark">{{ html_data }}</pre>
+  </section>
+</template>
+
+<script>
+
+const axiosObj = require('axios');
+//テスト用ダミーJSON;
+let url = "https://jsonplaceholder.typicode.com/posts";
+
+
+export default {
+  data: function() {
+    return {
+      title:'Axios',
+      message:'this page is test of axios.'
+    };
+  },
+  asyncData: async function() {
+    let result = await axiosObj.get(url);
+    return { html_data: result.data};
+  }
+}
+</script>
